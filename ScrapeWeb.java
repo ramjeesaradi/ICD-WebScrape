92f3e5e11df23f83f1ce49d612757597cd0253ba
Ramanuja Rao Kotaprolu <ramjeesaradi@gmail.com>
2016-03-06 19:54:07 +0530

    complete code.

--- ScrapeWeb.java
+++ ScrapeWeb.java
@@ -1,15 +1,13 @@
-/**
- * Created by hp on 4/16/2015.
- */
+
 import java.io.*;
 import java.net.*;
 import java.util.regex.*;
 
 public class ScrapeWeb {
-    public static void main(String[] args) throws IOException {
+    	static String getHTML(String urlSt) throws IOException {
         URL url = null;
         try {
-            url = new URL("http://postyourproblem.in");
+            url = new URL(urlSt);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -33,6 +31,29 @@ public class ScrapeWeb {
             buf.append((char) ch);
         }
         String str = buf.toString();
-        System.out.println(str);
+//        System.out.println(str);
+        return(str);
+    }
+    	static String getICD10(String pg){
+    		
+    		String ICD10 = null;
+//    		pg = pg.replaceAll("[\t\n\r]", "");
+//    		System.out.println(pg);
+    		System.out.println(pg.contains("ICD-10-PCS code "));
+    		Pattern p = Pattern.compile(".*ICD-10-PCS code ([^\\s]+)\".*",Pattern.DOTALL);
+    		
+    		Matcher m = p.matcher(pg);
+    		m.find();
+    		ICD10 = m.group(1);
+    		
+			return ICD10;
+    		
+    	}
+    public static void main(String[] args) throws IOException {
+    	String addr = "http://www.icd10data.com/Convert/";
+    	String ICD9 = "51.24";
+    	String pg = getHTML("http://www.icd10data.com/Convert/51.24");
+    	String ICD10 = getICD10(pg);
+    	System.out.println(ICD10);
     }
 }
